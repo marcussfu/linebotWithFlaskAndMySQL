6ccc2879dade21333b7072b77521b6b104d4b3fb
marcusfu <marcussfu@gmail.com>
2020-09-30 00:21:54 +0800

    test select string

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def handle_message(event):
     """
 
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, message+sql)
 
 import os
 if __name__ == "__main__":
